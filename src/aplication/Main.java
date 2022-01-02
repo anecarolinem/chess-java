9977366a08cd84384d08233fcf5eae5226da1e59
anecarolinem <anecarolinemaciel7@gmail.com>
2022-01-02 18:14:47 -0300

    create first class

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,9 +1,13 @@
 package aplication;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Position posicao = new Position (3,5);
+		System.out.println(posicao);
 
 	}
 
